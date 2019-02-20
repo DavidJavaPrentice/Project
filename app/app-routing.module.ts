4b8b48c4c1a8fdcc57b1d5223cee1f606561cd3f
David Prentice <46706997+DavidJavaPrentice@users.noreply.github.com>
2019-02-20 13:14:49 +0100

    Add files via upload

---
+++ app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+// import {BierListComponent} from "./bier-list/bier-list.component";
+import {LoginComponent} from "./login/login.component";
+import {UserFormComponent} from "./user-form/user-form.component";
+// import {HomepageComponent} from "./homepage/homepage.component";
+// import {RateComponent} from "./rate/rate.component";
+// import {GeschiedenisComponent} from "./geschiedenis/geschiedenis.component";
+// import {AchievementsComponent} from "./achievements/achievements.component";
+
+
+const routes: Routes = [
+  // {path: 'bier', component: BierListComponent},
+  {path: '', component: LoginComponent},
+  {path: 'registreren', component: UserFormComponent},
+  // {path: 'homepage', component: HomepageComponent},
+  // {path: 'rate', component: RateComponent},
+  // {path: 'geschiedenis', component: GeschiedenisComponent},
+  // {path: 'achievements', component: AchievementsComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+// import { BierListComponent } from './bier-list/bier-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {UserFormComponent} from "./user-form/user-form.component";
+import { LoginComponent } from './login/login.component';
+import { StorageServiceModule } from 'ngx-webstorage-service';
+import {LocalStorageService} from "./LocalStorageService";
+// import { HomepageComponent } from './homepage/homepage.component';
+// import { RateComponent } from './rate/rate.component';
+// import { AchievementsComponent } from './achievements/achievements.component';
+// import { GeschiedenisComponent } from './geschiedenis/geschiedenis.component';
+// import { NavbarComponent } from './navbar/navbar.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    // BierListComponent,
+    UserFormComponent,
+    LoginComponent,
+    // HomepageComponent,
+    // RateComponent,
+    // AchievementsComponent,
+    // GeschiedenisComponent,
+    // NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    StorageServiceModule,
+    MDBBootstrapModule.forRoot(),
+    MDBBootstrapModule.forRoot()
+  ],
+  providers: [LocalStorageService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
